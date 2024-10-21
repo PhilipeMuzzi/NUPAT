a2a2e5048d3927c8f0b60bd1279072fbe3fe5faf
vangoncalves <vangoncalvesilva@gmail.com>
2024-10-21 10:08:07 -0300

    vou comecar a FINALMENTE tentar um filtro pra mudar a situação dos projetos em uma pag so

--- core/views.py
+++ core/views.py
@@ -78,8 +78,16 @@ def atendimento_virtual(request):
     return render(request, 'Atendimento virtual/atendimento_virtual.html')
 
 # Listagem de TODOS OS projetos PARA TODOS OS USUÁRIOS:
-def listar_projetos_todos(request):
-    projetos = Projeto.objects.all()  # Obtém todos os projetos
+def listar_projetos_todos(request): 
+    pesquisa= request.GET.get('pesquisa')
+    projetos_andamento = Projeto.objects.filter(situacao='andamento')
+    projetos_concluidos = Projeto.objects.filter(situacao='concluido')
+    projetos_iniciados = Projeto.objects.filter(situacao='planejamento')
+    print(pesquisa)
+    if pesquisa:
+        projetos = Projeto.objects.filter(titulo__icontains=pesquisa)
+    else:
+        projetos = Projeto.objects.all()
     return render(request, 'projetos/listar_todos_os_projetos.html', {'projetos': projetos})
 
 # Projetos marcados pelo admin como "Em Andamento"
