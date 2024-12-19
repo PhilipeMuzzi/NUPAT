272ccc400d6bdb8a0e25b3376f0acc73bc6b7303
Philipe Muzzi <phmuzzi10@gmail.com>
2024-12-18 21:12:06 -0300

    Pacote de atualizações finais parte 1

--- core/auth_backend.py
+++ core/auth_backend.py
@@ -1,6 +1,5 @@
-# core/auth_backend.py
-from django.contrib.auth.models import User
 from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
@@ -8,5 +7,11 @@ class EmailBackend(ModelBackend):
             user = User.objects.get(email=username)
             if user.check_password(password):
                 return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
